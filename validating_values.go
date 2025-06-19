7bc96d928a54408a0f46387a97a4c54893c9d8c8
am <am@acorello.it>
2025-06-19 16:51:00 +0100

    Introduce simpler UrlValues helper
    Delegates all sanitisation and validation to the provided
    parser `func(string)error`.

--- validating_values.go
+++ validating_values.go
@@ -1,9 +1,11 @@
 package uttpil
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -46,4 +48,62 @@ func (my UrlValues) Trim_NotBlank(name string) (string, error) {
 		return "", fmt.Errorf("%q is blank or empty", name)
 	}
 	return v, nil
+}
+
+type UrlValuesHelper struct {
+	url.Values
+	errors map[string]error
+}
+
+func NewUrlValuesHelper(r *http.Request) (values UrlValuesHelper, err error) {
+	err = r.ParseForm()
+	if err != nil {
+		return values, err
+	}
+	return UrlValuesHelper{
+		Values: r.Form,
+		errors: make(map[string]error),
+	}, nil
+}
+
+// Give provides value for `key` to the parser. Error returned is collected and all errors
+// should be retrieved with Errors()
+//
+// Example:
+//
+//	    var person Person
+//
+//		form.Give("FirstName", func(name string) error {
+//			name = strings.TrimSpace(name) // sanitise
+//			if name == "" {				   // validate
+//				return fmt.Errorf("blank")
+//			}
+//			person.FirstName = name
+//			return nil
+//		})
+//
+//		form.Errors()
+func (my UrlValuesHelper) Give(key string, parser func(string) error) {
+	if !my.Has(key) {
+		my.joinError(key, fmt.Errorf("not found"))
+		return
+	}
+	err := parser(my.Get(key))
+	my.joinError(key, err)
+}
+
+func (my UrlValuesHelper) Get(name string, sanitizers ...func(string) string) string {
+	v := my.Values.Get(name)
+	for s := range slices.Values(sanitizers) {
+		v = s(v)
+	}
+	return v
+}
+
+func (my UrlValuesHelper) Errors() map[string]error {
+	return my.errors
+}
+
+func (my UrlValuesHelper) joinError(key string, err error) {
+	my.errors[key] = errors.Join(my.errors[key], err)
 }
