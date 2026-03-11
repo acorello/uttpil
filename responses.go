c7898ee550e24835576c0990b472583f6dbc3b93
am <am@acorello.it>
2026-03-11 17:06:20 +0000

    Change response code to 405 if no handler for method was found

--- responses.go
+++ responses.go
@@ -6,6 +6,6 @@ import (
 )
 
 func RespondErrMethodNotImplemented(w http.ResponseWriter, r *http.Request) {
-	msg := fmt.Sprintf("method %q not implemented for %q", r.Method, r.URL)
-	http.Error(w, msg, http.StatusNotImplemented)
+	msg := fmt.Sprintf("method %q not allowed", r.Method)
+	http.Error(w, msg, http.StatusMethodNotAllowed)
 }
